2356562335e458d3f398fe97850d2cf9354eb84e
notCactus <cactusdevcode@gmail.com>
2020-02-15 13:27:42 +0100

    added movement, scoring and sound effects

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 mod pong;
+mod systems;
+mod audio;
 
 use crate::pong::Pong;
 use amethyst::{
     core::TransformBundle,
+    input::{InputBundle, StringBindings},
     prelude::*,
     renderer::{
         plugins::{RenderFlat2D, RenderToWindow},
@@ -10,6 +13,8 @@ use amethyst::{
         RenderingBundle,
     },
     utils::application_root_dir,
+    ui::{RenderUi, UiBundle},
+    audio::AudioBundle,
 };
 fn main() -> amethyst::Result<()> {
     // Logger
@@ -18,23 +23,40 @@ fn main() -> amethyst::Result<()> {
     let app_root = application_root_dir()?;
     let display_config_path = app_root.join("config").join("display.ron");
 
+    let binding_path = app_root.join("config").join("bindings.ron");
+
+    let input_bundle =
+        InputBundle::<StringBindings>::new().with_bindings_from_file(binding_path)?;
+
     let assets_dir = app_root.join("assets");
 
     let mut world = World::new();
     let game_data = GameDataBuilder::default()
-    .with_bundle(TransformBundle::new())?
-    .with_bundle(
-        RenderingBundle::<DefaultBackend>::new()
-            // The RenderToWindow plugin provides all the scaffolding for opening a window and drawing on it
-            .with_plugin(
-                RenderToWindow::from_config_path(display_config_path)?
-                    .with_clear([0.0, 0.0, 0.0, 1.0]),
-            )
-            // RenderFlat2D plugin is used to render entities with a `SpriteRender` component.
-            .with_plugin(RenderFlat2D::default()),
-    )?;
+        .with_bundle(TransformBundle::new())?
+        .with_bundle(input_bundle)?
+        .with_bundle(UiBundle::<StringBindings>::new())?
+        .with_bundle(AudioBundle::default())?
+        .with(systems::PaddleSystem, "paddle_system", &["input_system"])
+        .with(systems::MoveBallsSystem, "ball_system", &[])
+        .with(
+            systems::BounceSystem,
+            "collision_system",
+            &["paddle_system", "ball_system"],
+        )
+        .with(systems::WinnerSystem, "winner_system", &["ball_system"])
+        .with_bundle(
+            RenderingBundle::<DefaultBackend>::new()
+                // The RenderToWindow plugin provides all the scaffolding for opening a window and drawing on it
+                .with_plugin(
+                    RenderToWindow::from_config_path(display_config_path)?
+                        .with_clear([0.0, 0.0, 0.0, 1.0]),
+                )
+                // RenderFlat2D plugin is used to render entities with a `SpriteRender` component.
+                .with_plugin(RenderFlat2D::default())
+                .with_plugin(RenderUi::default()),
+        )?;
 
-    let mut game = Application::new(assets_dir, Pong, game_data)?;
+    let mut game = Application::new(assets_dir, Pong::default(), game_data)?;
     game.run();
 
     Ok(())
---
+++ src/systems/winner.rs
@@ -0,0 +1,79 @@
+use amethyst::{
+    core::transform::Transform,
+    core::SystemDesc,
+    derive::SystemDesc,
+    ecs::prelude::{Join, ReadExpect, System, SystemData, World, Write, WriteStorage},
+    ui::UiText,
+    assets::AssetStorage,
+    audio::{output::Output, Source},
+    ecs::Read,
+};
+
+use crate::audio::{play_score_sound, Sounds};
+use std::ops::Deref;
+
+use crate::pong::{Ball, ScoreBoard, ScoreText, ARENA_WIDTH};
+
+#[derive(SystemDesc)]
+pub struct WinnerSystem;
+
+impl<'s> System<'s> for WinnerSystem {
+    type SystemData = (
+        WriteStorage<'s, Ball>,
+        WriteStorage<'s, Transform>,
+        WriteStorage<'s, UiText>,
+        Write<'s, ScoreBoard>,
+        ReadExpect<'s, ScoreText>,
+        Read<'s, AssetStorage<Source>>,
+        ReadExpect<'s, Sounds>,
+        Option<Read<'s, Output>>,
+    );
+
+
+    fn run(&mut self, (
+        mut balls,
+        mut locals,
+        mut ui_text,
+        mut scores,
+        score_text,
+        storage,
+        sounds,
+        audio_output,
+    ): Self::SystemData)  {
+        for (ball, transform) in (&mut balls, &mut locals).join() {
+            let ball_x = transform.translation().x;
+
+            let did_hit = if ball_x <= ball.radius {
+                // Right player scored on the left side.
+                // We top the score at 999 to avoid text overlap.
+                scores.score_right = (scores.score_right + 1).min(999);
+                if let Some(text) = ui_text.get_mut(score_text.p2_score) {
+                    text.text = scores.score_right.to_string();
+                }
+                true
+            } else if ball_x >= ARENA_WIDTH - ball.radius {
+                // Left player scored on the right side.
+                // We top the score at 999 to avoid text overlap.
+                scores.score_left = (scores.score_left + 1).min(999);
+                if let Some(text) = ui_text.get_mut(score_text.p1_score) {
+                    text.text = scores.score_left.to_string();
+                }
+                true
+            } else {
+                false
+            };
+
+            if did_hit {
+                ball.velocity[0] = -ball.velocity[0]; // Reverse Direction
+                transform.set_translation_x(ARENA_WIDTH / 2.0); // Reset Position
+                play_score_sound(&*sounds, &storage, audio_output.as_ref().map(|o| o.deref()));
+
+                // Print the scoreboard.
+                println!(
+                    "Score: | {:^3} | {:^3} |",
+                    scores.score_left, scores.score_right
+                );
+            }
+        }
+    }
+}
